cc95dfe19c26cc42e2ad2cc78f32540fe5c2ef35
Jiri Pechanec <jpechane@redhat.com>
2015-09-21 10:08:41 +0200

    Removed obosleted non-modularized code

--- src/main/java/com/redhat/brq/integration/exercise/accounting/RESTApplication.java
+++
@@ -1,9 +0,0 @@
-package com.redhat.brq.integration.exercise.accounting;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("rest")
-public class RESTApplication extends Application {
-
-}
