92955b1a4b8f210eea80d2cacb51d77ef1e7af87
Jiri Pechanec <jpechane@redhat.com>
2015-10-06 08:26:54 +0200

    Allowed access to db from remote IPs

--- docker/launch.sh
+++ docker/launch.sh
@@ -4,5 +4,5 @@ cd /root/wildfly-9.0.1.Final
 #sleep 5
 #bin/jboss-cli.sh -c --command='data-source add --name=ExaminationDS --jndi-name=java:jboss/datasources/ExaminationDS --use-java-context=true --driver-name=h2 --connection-url=jdbc:h2:tcp://localhost/~/exam --user-name=sa --password=sa --spy=true --pool-prefill=true --min-pool-size=10 --max-pool-size=20'
 #bin/jboss-cli.sh -c --command='shutdown'
-java -cp modules/system/layers/base/com/h2database/h2/main/h2*.jar org.h2.tools.Server -webAllowOthers &
+java -cp modules/system/layers/base/com/h2database/h2/main/h2*.jar org.h2.tools.Server -webAllowOthers -tcpAllowOthers &
 bin/standalone.sh -b 0.0.0.0
